b6c2b797ea2228c871886658775ef8766dce4eb3
Diogo Cordeiro <diogo@diogocordeiro.pt>
2021-10-08 22:33:48 +0100

    aligned sort in api get cards with main index page

--- pages/api/cards/index.js
+++ pages/api/cards/index.js
@@ -9,7 +9,7 @@ export default async (req, res) => {
     const client = await clientPromise;
     const collection = await client.db().collection('cards');
     try {
-      let mySort= {createdOn:-1};
+      let mySort= {createdOn:-1, lastModified: -1, cardText: 1};
       const cards= await collection.find().sort(mySort).toArray();
       res.json(cards);
 
