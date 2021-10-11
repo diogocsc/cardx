27fd1b643938fac7a99ce69806cbbda745b3d927
Diogo Cordeiro <diogo@diogocordeiro.pt>
2021-10-11 21:08:43 +0100

    Passing orderBy directly to form submit function

--- pages/cards/cardEdit.js
+++ pages/cards/cardEdit.js
@@ -12,41 +12,41 @@ import { Text } from "react";
 export default function Form() {
     const router = useRouter()
     const cardId = router.query.id
-    const submitCard = async event => {
-      event.preventDefault()
-      const res = cardId ? await fetch(
-        '/api/cards/'+cardId,
-        {
-          body: JSON.stringify({
-            cardText: event.target.cardText.value,
-            category: event.target.category.value,
-            cardUsers: event.target.cardUsers.value,
-            source: event.target.source.value,
-            ownedBy: event.target.ownedBy.value.split(","),
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          method: 'PATCH'
-        }
-      ) : await fetch(
-        '/api/cards/insertCard',
-        {
-          body: JSON.stringify({
-            cardText: event.target.cardText.value,
-            category: event.target.category.value,
-            cardUsers: event.target.cardUsers.value,
-            source: event.target.source.value
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          method: 'POST'
-        }
-      )
-      alert("Card Submitted");
+    async function submitCard(event, ownedBy) {
+    event.preventDefault()
+    const res = cardId ? await fetch(
+      '/api/cards/' + cardId,
+      {
+        body: JSON.stringify({
+          cardText: event.target.cardText.value,
+          category: event.target.category.value,
+          cardUsers: event.target.cardUsers.value,
+          source: event.target.source.value,
+          ownedBy: ownedBy,
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'PATCH'
+      }
+    ) : await fetch(
+      '/api/cards/insertCard',
+      {
+        body: JSON.stringify({
+          cardText: event.target.cardText.value,
+          category: event.target.category.value,
+          cardUsers: event.target.cardUsers.value,
+          source: event.target.source.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }
+    )
+    alert("Card Submitted")
 
-    } 
+  } 
 
     const [card, setCard] = useState('');
 
@@ -67,7 +67,7 @@ export default function Form() {
           <meta name="description" content="The place to edit or create cards" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <form onSubmit={submitCard}>
+        <form onSubmit={(event) => submitCard(event, card.ownedBy)}>
         <label className={utilStyles.input_label} htmlFor="cardText">Card Text</label>
 
         <div className={utilStyles.input}>
@@ -98,8 +98,6 @@ export default function Form() {
 
            <textarea className={utilStyles.input_field} cols="30" rows="3" id="source" name="source" type="text" defaultValue={card.source} />
           </div>
-
-            <input className={utilStyles.input_field} style={{display:"none"}} id="ownedBy" name="ownedBy" type="text" defaultValue={card.ownedBy} />
 
           <button className={utilStyles.card_button} type="submit">Submit</button>
           <br />
