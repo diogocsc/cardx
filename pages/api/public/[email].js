e91654d5a3511623d9a38959b919b7c2e784578d
Diogo Cordeiro <diogo@diogocordeiro.pt>
2021-10-12 18:27:30 +0100

    Added cross-origin feature (cors) to public api

--- pages/api/public/[email].js
+++ pages/api/public/[email].js
@@ -1,11 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 "use strict";
-
+import Cors from 'cors'
+import initMiddleware from '../../../lib/init-middleware'
 // Import the dependency.
 import clientPromise from '../../../mongodb-client';
 
 export default async (req, res) => {
 
+        // Initialize the cors middleware
+        const cors = initMiddleware(
+            // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+            Cors({
+            // Only allow requests with GET, POST and OPTIONS
+            methods: ['GET', 'POST', 'OPTIONS'],
+            //  methods: ['GET'],
+            })
+        ) 
+        await cors(req, res); 
         const email=Buffer.from(req.query.email, 'base64').toString();
         const client = await clientPromise;
         const collection = await client.db().collection('cards');
